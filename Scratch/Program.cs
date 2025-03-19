40f446545ea10a6742a21e5c2f1222f3229e7571
Angelo Davales <angelodavales0528@gmail.com>
2025-03-19 13:51:24 +0800

    push sol.

--- Scratch/Program.cs
+++ Scratch/Program.cs
@@ -25,16 +25,52 @@
 
 #region Arturo Fibonacci
 //0 1 1 2 3 5
-Console.WriteLine("Arturo" + ArturoFibbonacci(4));
-Console.WriteLine();
+//Console.WriteLine("Arturo" + ArturoFibbonacci(4));
+//Console.WriteLine();
 
-int ArturoFibbonacci(int n)
+//int ArturoFibbonacci(int n)
+//{
+//	if (n <= 1)
+//	{
+//		return n;
+//	}
+
+//    return ArturoFibbonacci(n - 1) + ArturoFibbonacci(n - 2);
+//}
+#endregion
+
+#region Sorting
+1 2 3 4
+int[] arr = new int[]{ 5,4,4,3,2,1};
+
+int left = 0;
+int right = left + 1;
+while (arr[left] > arr[right])
 {
-	if (n <= 1)
+	var temp = arr[left];
+	arr[left] = arr[right];
+	arr[right] = temp;
+	right++;
+	if (arr[right] < )
 	{
-		return n;
-	}
 
-    return ArturoFibbonacci(n - 1) + ArturoFibbonacci(n - 2);
+	}
 }
+
+//for (int i = 0; i < arr.Length; i++)
+//{
+//	for (int j = i + 1; j < arr.Length; j++)
+//	{
+//		if (arr[i] > arr[j])
+//		{
+//			Console.WriteLine("before: " + arr[i] + " " + arr[j]);
+//			var temp = arr[i];
+//			arr[i] = arr[j];
+//			arr[j] = temp;
+//            Console.WriteLine("after: " + arr[i] + " " + arr[j]);
+//        }
+//	}
+//}
+var test = arr;
+Console.WriteLine();
 #endregion
--- TreeNode/Program.cs
+++ TreeNode/Program.cs
@@ -1,18 +1,57 @@
-﻿public class TreeNode
+﻿
+public class TreeNode
 {
     public int? _val;
     public TreeNode _left;
     public TreeNode _right;
 
-    public TreeNode(int? val = null, TreeNode left = null, TreeNode right = null)
+    public TreeNode(int? val, TreeNode left = null, TreeNode right = null)
     {
         _val = val;
         _left = left;
         _right = right;
     }
 
-    public static void Main(string[] args)
+    #region BuildTree
+    public static TreeNode BuildTree(int?[] nums)
     {
+        if (nums.Length == 0 || nums[0] == null) return null;
 
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        TreeNode root = new TreeNode(nums[0]);
+        queue.Enqueue(root);
+
+        int i = 1;
+        while (i < nums.Length)
+        {
+            TreeNode current = queue.Dequeue();
+
+            // Left child
+            if (i < nums.Length && nums[i] != null)
+            {
+                current._left = new TreeNode(nums[i]);
+                queue.Enqueue(current._left);
+            }
+            i++;
+
+            // Right child
+            if (i < nums.Length && nums[i] != null)
+            {
+                current._right = new TreeNode(nums[i]);
+                queue.Enqueue(current._right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+    #endregion
+
+    static void Main()
+    {
+        int?[] values = { 5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1 };
+        var tree = BuildTree(values);
+
+        
     }
 }
